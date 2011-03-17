38657294b9588daac1191d120930b89f2a8f26f8
Michael Knoll <mimi@kaktusteam.de>
2011-03-17 16:19:45 +0100

    TASK: Initial commit

---
+++ ext_tables.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined ('TYPO3_MODE')) die ('Access denied.');
+
+
+
+
+
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Tools for Extbase development');
+
+
+
+
+
+?>
