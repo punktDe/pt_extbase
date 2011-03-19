83b11db532521ed318307bc7eab4882554cd7f4e
Michael Knoll <mimi@kaktusteam.de>
2011-03-19 21:41:57 +0100

    ADD: SessionPersistableInterface

---
+++ Classes/State/Session/SessionPersistableInterface.php
@@ -0,0 +1,53 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2010-2011 punkt.de GmbH - Karlsruhe, Germany - http://www.punkt.de
+ *  Authors: Daniel Lienert, Michael Knoll, Christoph Ehscheidt
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Interface for objects to be persistable in sessions
+ *
+ * @package Domain
+ * @subpackage StateAdapter
+ * @author Michael Knoll 
+ */
+interface Tx_PtExtbase_State_Session_SessionPersistableInterface extends Tx_PtExtbase_State_IdentifiableInterface {
+	
+	/**
+	 * Called by any mechanism to persist an object's state to session
+	 *
+	 * @return array Object's state to be persisted to session
+	 */
+    public function persistToSession();
+    
+    
+    
+    /**
+     * Called by any mechanism to inject an object's state from session
+     *
+     * @param array $sessionData Object's state previously persisted to session
+     */
+    public function injectSessionData(array $sessionData);
+	
+}
+
+?>
