f2a72c738be35474808639b1059513fa8f47ba9b
Daniel Lienert <daniel@lienert.cc>
2011-03-22 08:52:48 +0100

    ADD: LCHookManager Hook

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,9 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+
+/**
+ * Register LifeCycle Manager
+ */
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['hook_eofe'][] = 'EXT:pt_extbase/Classes/Lifecycle/HookManager.php:tx_PtExtbase_Lifecycle_HookManager->updateEnd';
 ?>
