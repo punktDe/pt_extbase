793a63326bf15bd55524d7e0cf6a41efe3f81937
Michael Knoll <mimi@kaktusteam.de>
2012-01-27 19:41:00 +0100

    ADD: Added TCA and SQL definitions for tree / node stuff

--- ext_tables.sql
+++ ext_tables.sql
@@ -8,6 +8,8 @@ CREATE TABLE tx_ptextbase_cache_state (
    KEY cache_id (`identifier`)
 );
 
+
+
 CREATE TABLE tx_ptextbase_cache_state_tags (
    id int(11) NOT NULL auto_increment,
    identifier varchar(128) NOT NULL DEFAULT '',
@@ -15,4 +17,42 @@ CREATE TABLE tx_ptextbase_cache_state_tags (
    PRIMARY KEY (id),
    KEY cache_id (`identifier`),
    KEY cache_tag (`tag`)
+);
+
+
+
+CREATE TABLE tx_ptextbase_tree_node (
+    uid int(11) NOT NULL auto_increment,
+    pid int(11) DEFAULT '0' NOT NULL,
+
+    name  tinytext,
+    description text,
+    lft   int(11) DEFAULT '0' NOT NULL,
+    rgt   int(11) DEFAULT '0' NOT NULL,
+    root  int(11) DEFAULT '0' NOT NULL,
+
+    tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+
+    t3ver_oid int(11) DEFAULT '0' NOT NULL,
+    t3ver_id int(11) DEFAULT '0' NOT NULL,
+    t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+    t3ver_label varchar(30) DEFAULT '' NOT NULL,
+    t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+    t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
+    t3ver_count int(11) DEFAULT '0' NOT NULL,
+    t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+    t3_origuid int(11) DEFAULT '0' NOT NULL,
+
+    sys_language_uid int(11) DEFAULT '0' NOT NULL,
+    l18n_parent int(11) DEFAULT '0' NOT NULL,
+    l18n_diffsource mediumblob NOT NULL,
+
+    PRIMARY KEY (uid),
+    KEY parent (pid),
+    KEY lft (lft),
+    KEY rgt (rgt),
+    KEY root (root)
 );
