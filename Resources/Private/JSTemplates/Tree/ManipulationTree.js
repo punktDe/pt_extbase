711d7e31ea6f43ce793aa4f8a7ff3f2d4d2eedad
Sebastian Helzle <helzle@punkt.de>
2012-08-02 10:52:54 +0200

    CHG: JS Tree now supports jstree. Fixed some bugs with the domain model for the tree

--- Resources/Private/JSTemplates/Tree/ManipulationTree.js
+++ Resources/Private/JSTemplates/Tree/ManipulationTree.js
@@ -13,90 +13,161 @@
 		baseRequest = {
 			extensionName: 'ptExtbase',
 			pluginName: 'ptx',
-			controllerName: 'Tree'
+			controllerName: 'Tree',
+			actionName: '',
+			arguments: {}
 		};
 
+	function log(message) {
+		if (window.console && debug)
+			window.console.log(message);
+	}
+
+	/**
+	 * Creates a request data object
+	 */
+	function createRequestData(action, arguments) {
+		if (!arguments) arguments = {};
+
+		return $.extend(true, {}, baseRequest, { actionName: action, arguments: arguments });
+	}
+
+	/**
+	 * Options for the jstree jQuery plugin
+	 */
 	var treeOptions = {
+		'plugins': ['themes', 'json_data', 'ui', 'cookies', 'dnd', 'contextmenu', 'crrm'],
 		'json_data': {
 			//'progressive_render' : true,
 			'ajax': {
 				'url': baseURL,
 				'data': function(node) {
-					log('Requesting tree data for ' + node);
-					return $.extend({}, baseRequest, {
-						"actionName" : "getTree"
-					});
+					log('Requesting tree data for node id ' + node);
+					return createRequestData("getTree");
 				}
 			}
 		},
-		'plugins': ['themes', 'json_data', 'ui', 'cookies', 'dnd']
+		'contextmenu': {
+			'items': {
+				'create': {
+					'label': 'Neue Unterkategorie'
+				},
+				'rename': {
+					'label': 'Umbenennen'
+				},
+				'ccp': {
+					'label': 'Bearbeiten'
+				},
+				'remove': {
+					'label': 'Loeschen'
+				}
+			}
+		}
 	};
-
-	function log(message) {
-		window.console && debug ? window.console.log(message) : alert(message);
-	}
 
 	function moveNode(e, data) {
 		data.rslt.o.each(function(i) {
-
 			var node = $(this),
 				nodeId = node.attr('id'),
 				newPosition = data.rslt.cp + i,
-				targetNode = undefined,
-				parent = $(data.rslt.cr),
-				actionName = 'test';//'moveNodeAfter';
+				requestData = createRequestData(undefined, { node: nodeId }),
+				parent = $(data.rslt.cr);
 
-			if (newPosition == 1) {
-				// Check if node has a sibling next to it
-				var next = node.next();
-				if (next.length) {
-					actionName = 'moveNodeBefore';
-					targetNode = next.attr('id');
-				} else {
-					actionName = 'moveNodeInto';
-					targetNode = parent.attr('id');
-				}
-			} else if (newPosition > 1) {
+			if (newPosition > 1) {
 				// Check if node has a sibling before
 				var prev = node.prev();
 				if (prev.length) {
-					actionName = 'moveNodeAfter';
-					targetNode = prev.attr('id');
+					requestData.actionName = 'moveNodeAfter';
+					requestData.arguments.targetNode = prev.attr('id');
 				} else {
-					actionName = 'moveNodeInto';
-					targetNode = parent.attr('id');
+					requestData.actionName = 'moveNodeInto';
+					requestData.arguments.targetNode = parent.attr('id');
 				}
 			} else {
-				actionName = 'moveNodeInto';
-				targetNode = parent.attr('id');
+				// Check if node has a sibling next to it
+				var next = node.next();
+				if (next.length) {
+					requestData.actionName = 'moveNodeBefore';
+					requestData.arguments.targetNode = next.attr('id');
+				} else {
+					requestData.actionName = 'moveNodeInto';
+					requestData.arguments.targetNode = parent.attr('id');
+				}
 			}
 
-			log(actionName + " node " + nodeId + " to position " + newPosition);
-			log("Node parent is " + parent.attr('id'));
+			log('Data for request:');
+			log(requestData);
 
 			// Send move request
-			$.ajax({
-				async : false,
-                type: 'get',
-                url: baseURL,
-                data : $.extend({}, baseRequest, {
-                    actionName : actionName,
-                	arguments: {
-	                    node : nodeId,
-	                    targetNode: targetNode
-                	}
-                }),
-                success : function (r) {
-                	log(r);
-                    /*if(!r.status) {
-                        $.jstree.rollback(data.rlbk);
-                    } else {
-                        $(data.rslt.oc).attr("id", r.id);
-                        if(data.rslt.cy && $(data.rslt.oc).children("ul").length)
-                            data.inst.refresh(data.inst._get_parent(data.rslt.oc));
-                    }*/
-                }
+			if (requestData.actionName !== undefined) {
+				$.getJSON(baseURL, requestData, function (r) {
+					log('MoveNode Success: ' + r.status);
+
+					if(!r.status)
+						$.jstree.rollback(data.rlbk);
+					else {
+						$(data.rslt.oc).attr("id", r.id);
+						if(data.rslt.cy && $(data.rslt.oc).children("ul").length)
+							data.inst.refresh(data.inst._get_parent(data.rslt.oc));
+					}
+				});
+			} else {
+				log('Error: action undefined');
+			}
+		});
+	}
+
+	function createNode(e, data) {
+		var newNode = $(data.rslt.obj),
+			parent = $(data.rslt.parent),
+			position = data.rslt.position,
+			requestData = createRequestData('addNode', {
+				parent: parent.attr('id'),
+				label: data.rslt.name
 			});
+
+		log(requestData);
+
+		$.getJSON(baseURL, requestData, function (r) {
+			log('CreateNode Success: ' + r.status);
+
+			if(r.status)
+				$(data.rslt.obj).attr("id", r.id);
+			else
+				$.jstree.rollback(data.rlbk);
+		});
+	}
+
+	function removeNode(e, data) {
+		var node = $(data.rslt.obj),
+			requestData = createRequestData('removeNode', {
+				node: node.attr("id")
+			});
+
+		log(requestData);
+
+		$.getJSON(baseURL, requestData, function (r) {
+			log('RemoveNode Success: ' + r.status);
+			
+			if(!r.status)
+                data.inst.refresh();
+		});
+	}
+
+	function renameNode(e, data) {
+		var node = $(data.rslt.obj),
+			requestData = createRequestData('saveNode', {
+				node: node.attr("id"),
+				label: data.rslt.new_name
+			});
+
+		log(requestData);
+
+		$.getJSON(baseURL, requestData, function (r) {
+			log('RenameNode Success: ' + r.status);
+
+			if(!r.status)
+				$.jstree.rollback(data.rlbk);
 		});
 	}
 
@@ -104,6 +175,9 @@
 		// Load data and init tree
 		log('Creating tree');
 		jsTreeInstance = $(treeDiv).jstree(treeOptions)
-			.bind("move_node.jstree", moveNode);
+			.bind("move_node.jstree", moveNode)
+			.bind("create.jstree", createNode)
+			.bind("remove.jstree", removeNode)
+			.bind("rename.jstree", renameNode);
 	});
 })(jQuery);
