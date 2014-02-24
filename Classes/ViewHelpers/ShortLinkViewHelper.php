7d2bc43c11615257db375e4411f9edbdc0b94451
Christian Herberger <herberger@punkt.de>
2014-02-24 15:14:11 +0100

    ADD: viewhelper for short link

---
+++ Classes/ViewHelpers/ShortLinkViewHelper.php
@@ -0,0 +1,48 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 punkt.de <el_equipo@punkt.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Request Arguments ViewHelper
+ *
+ * @package pt_extbase
+ * @subpackage ViewHelpers
+ */
+class Tx_PtExtbase_ViewHelpers_ShortLinkViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * @param int $length
+	 * @param string $indexScriptUrl
+	 *
+	 * @return string
+	 */
+	public function render($length = 0, $indexScriptUrl = '') {
+		$link = $this->renderChildren();
+
+		$shortLink = t3lib_div::makeRedirectUrl($link, $length, $indexScriptUrl);
+
+		return $shortLink;
+
+	}
+
+}
