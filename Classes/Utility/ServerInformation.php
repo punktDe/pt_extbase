a4a87467c7631efdd658ec692d1ad4d7df6e11e6
Joachim Mathes <mathes@punkt.de>
2014-12-09 12:29:25 +0100

    [ADD] ServerInformation utility class

---
+++ Classes/Utility/ServerInformation.php
@@ -0,0 +1,45 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 punkt.de GmbH
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Server information
+ *
+ * @package pt_extbase
+ * @subpackage Classes\Utility
+ */
+class Tx_PtExtbase_Utility_ServerInformation implements \TYPO3\CMS\Core\SingletonInterface {
+
+	/**
+	 * @return string
+	 */
+	public function getServerHostName() {
+		foreach (array('HOSTNAME', 'HTTP_HOST') as $key) {
+			if (array_key_exists($key, $_SERVER)) {
+				return $_SERVER[$key];
+			}
+		}
+		return '';
+	}
+
+}
