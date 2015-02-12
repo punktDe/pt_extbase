98cc5514a051b1f72293a4f69e584de00e54a423
Ursula Klinger <klinger@punkt.de>
2015-02-12 09:03:15 +0100

    [CHG] add component to log message

--- Classes/Controller/AbstractApiController.php
+++ Classes/Controller/AbstractApiController.php
@@ -65,9 +65,9 @@ class Tx_PtExtbase_Controller_AbstractApiController extends \TYPO3\CMS\Extbase\M
 			$this->response->setContent($exception->getCode());
 
 			if ($exception instanceof Tx_PtExtbase_Exception_LoggerException) {
-				$this->logger->log($exception->getLogLevel(), sprintf('%s (%s)', $exception->getMessage(), $exception->getCode()));
+				$this->logger->log($exception->getLogLevel(), sprintf('%s (%s)', $exception->getMessage(), $exception->getCode()), __CLASS__);
 			} else {
-				$this->logger->error(sprintf('%s (%s)', $exception->getMessage(), $exception->getCode()));
+				$this->logger->error(sprintf('%s (%s)', $exception->getMessage(), $exception->getCode()), __CLASS__);
 			}
 
 			$this->objectManager->get('TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager')->persistAll();
