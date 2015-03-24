dfcbe5f87e1db8fd516d211c57a4b00b3689b4d7
Joachim Mathes <mathes@punkt.de>
2015-03-24 09:09:16 +0100

    [ADD] Add tag pushing functionality to git wrapper

--- Tests/Unit/Utility/Git/GitRepositoryTest.php
+++ Tests/Unit/Utility/Git/GitRepositoryTest.php
@@ -167,6 +167,7 @@ class GitRepositoryTest extends UnitTestCase {
 			->execute();
 
 		$this->proxy->push()
+			->setTags()
 			->setRemote('origin')
 			->setRefspec('master')
 			->execute();
@@ -215,7 +216,7 @@ class GitRepositoryTest extends UnitTestCase {
 				array($this->equalTo('cd ~; /usr/bin/git --git-dir=~/.git remote remove origin')),
 				array($this->equalTo('cd ~; /usr/bin/git --git-dir=~/.git remote add origin file:///tmp/punktde.git')),
 				array($this->equalTo('cd ~; /usr/bin/git --git-dir=~/.git init --bare --shared')),
-				array($this->equalTo('cd ~; /usr/bin/git --git-dir=~/.git push origin master')),
+				array($this->equalTo('cd ~; /usr/bin/git --git-dir=~/.git push --tags origin master')),
 				array($this->equalTo('cd ~; /usr/bin/git --git-dir=~/.git tag -s -a -m "Release" v1.2.3')),
 				array($this->equalTo('cd ~; /usr/bin/git --git-dir=~/.git commit --message "This is a very cool message!"')),
 				array($this->equalTo('cd ~; /usr/bin/git --git-dir=~/.git add --all .')),
