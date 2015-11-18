9afab0efaa5d7a339c08489f574410de9a54db2d
Christian Herberger <herberger@punkt.de>
2015-11-18 17:37:29 +0100

    [ADD] FileTypeIconViewHelper

---
+++ Classes/ViewHelpers/FileTypeIconViewHelper.php
@@ -0,0 +1,59 @@
+<?php
+namespace PunktDe\PtExtbase\ViewHelpers;
+
+/***************************************************************
+ *  Copyright (C) 2015 punkt.de GmbH
+ *  Authors: el_equipo <opiuqe_le@punkt.de>
+ *
+ *  This script is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Class fileTypeIcon
+ *
+ * @package PunktDe/PtExtbase/ViewHelpers
+ */
+class FileTypeIconViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper
+{
+    /**
+     * @var string
+     */
+    protected $tagName = 'img';
+
+    /**
+     * @param string $fileExtension
+     * @param string $iconBaseDirectory
+     * @param string $iconExtension
+     * @return null||string
+     */
+    public function render($fileExtension, $iconBaseDirectory = '', $iconExtension = 'gif')
+    {
+        if ($iconBaseDirectory === '') {
+            $iconBaseDirectory = TYPO3_mainDir . 'gfx/fileicons/';
+        }
+
+        $iconPath = $iconBaseDirectory . $fileExtension . '.' . $iconExtension;
+
+        if (file_exists($iconPath)) {
+            $this->tag->addAttribute('src', $iconPath);
+            return $this->tag->render();
+        } else {
+            return null;
+        }
+
+    }
+
+}
