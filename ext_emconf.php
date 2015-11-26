aafcdc1ca55c98f3e86ab20ea381e79828db9bf7
Daniel Lienert <daniel@lienert.cc>
2015-11-26 18:35:15 +0100

    [TASK] Update mmaximum compatibility to 8.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '6.2.0-7.0.99',
+      'typo3' => '6.2.0-8.0.99',
     ),
     'conflicts' => 
     array (
