02dd078d1ee62b00c28067f2e8e1df1af635e154
Christian Herberger <herberger@punkt.de>
2016-07-18 16:55:06 +0200

    TASK: use class alias map to get non-namespaced classes extending namespaced
    classAliasMap is done like in tx_news and the function was validated studying the
    TYPO3 sources...

---
+++ Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'Tx_PtExtbase_Utility_TcaManager' => 'PunktDe\\PtExtbase\\Utility\\TcaManager',
+    'Tx_PtExtbase_Logger_Logger' => 'PunktDe\\PtExtbase\\Logger\\Logger',
+    'Tx_PtExtbase_Logger_LoggerConfiguration' => 'PunktDe\\PtExtbase\\Logger\\LoggerConfiguration',
+    'Tx_PtExtbase_Utility_ExtensionSettings' => 'PunktDe\\PtExtbase\\Utility\\ExtensionSettings',
+    'Tx_PtExtbase_Utility_FakeFrontendFactory' => 'PunktDe\\PtExtbase\\Utility\\FakeFrontendFactory',
+    'Tx_PtExtbase_Exception_ConfigurationException' => 'PunktDe\\PtExtbase\\Exception\\ConfigurationException',
+    'Tx_PtExtbase_Exception_LoggerException' => 'PunktDe\\PtExtbase\\Exception\\LoggerException',
+    'Tx_PtExtbase_Exception_Internal' => 'PunktDe\\PtExtbase\\Exception\\InternalException',
+    'Tx_PtExtbase_Exception_Exception' => 'PunktDe\\PtExtbase\\Exception\\Exception',
+    'Tx_PtExtbase_Configuration_AbstractConfiguration' => 'PunktDe\\PtExtbase\\Configuration\\AbstractConfiguration',
+    'Tx_PtExtbase_Configuration_AbstractConfigurationBuilder' => 'PunktDe\\PtExtbase\\Configuration\\AbstractConfigurationBuilder',
+    'Tx_PtExtbase_Utility_Namespace' => 'PunktDe\\PtExtbase\\Utility\\NamespaceUtility',
+    'Tx_PtExtbase_Collection_Collection' => 'PunktDe\\PtExtbase\\Collection\\Collection',
+    'Tx_PtExtbase_Collection_ObjectCollection' => 'PunktDe\\PtExtbase\\Collection\\ObjectCollection',
+    'Tx_PtExtbase_Tests_Unit_AbstractBaseTestcase' => 'PunktDe\\PtExtbase\\Testing\\Unit\\AbstractBaseTestcase',
+    'Tx_PtExtbase_Tests_Unit_AbstractModelTestcase' => 'PunktDe\\PtExtbase\\Testing\\Unit\\AbstractBaseTestcase',
+
+];
