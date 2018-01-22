8e21e159ff556e8e25ba0f5c7b5970affdee9907
Christian Herberger <herberger@punkt.de>
2018-01-22 13:49:25 +0100

    TASK: add isNumeric view helper

---
+++ Classes/ViewHelpers/IsNumericViewHelper.php
@@ -0,0 +1,39 @@
+<?php
+namespace PunktDe\PtExtbase\ViewHelpers;
+
+/***************************************************************
+ *  Copyright (C) 2017 punkt.de GmbH
+ *  Authors: el_equipo <el_equipo@punkt.de>
+ *
+ *  This script is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class IsNumericViewHelper extends AbstractViewHelper {
+
+    public function initializeArguments()
+    {
+        $this->registerArgument('value', 'mixed', 'the value to check', true);
+    }
+
+    /**
+     * @return boolean
+     */
+    public function render() {
+        return is_numeric($this->arguments['value']);
+    }
+}
