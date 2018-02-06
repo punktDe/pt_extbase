5d3eb93d8f584f382f4b304bab2b2b15cfad6150
Ursula Klinger <klinger@punkt.de>
2018-02-06 14:05:14 +0100

    BUGFIX: fix unit test

--- Tests/Unit/ViewHelpers/Format/FileSizeViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Format/FileSizeViewHelperTest.php
@@ -1,28 +1,15 @@
 <?php
-/***************************************************************
- * Copyright notice
- *
- *   2011 Daniel Lienert <daniel@lienert.cc>, Michael Knoll <mimi@kaktusteam.de>
- * All rights reserved
- *
- *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+namespace PunktDe\PtExtbase\Tests\Unit\ViewHelpers\Format;
 
+/*
+ * This file is part of the PunktDe\PtExtbase package.
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use PunktDe\PtExtbase\ViewHelpers\Format\FileSizeViewHelper;
 
 /**
  *
@@ -30,7 +17,7 @@
  * @subpackage ViewHelpers/Format
  * @author Daniel Lienert
  */
-class Tx_PtExtbase_Tests_Unit_ViewHelpers_Format_FileSizeViewHelperTest extends \PunktDe\PtExtbase\Testing\Unit\AbstractBaseTestcase
+class FileSizeViewHelperTest extends \PunktDe\PtExtbase\Testing\Unit\AbstractBaseTestcase
 {
     /**
      *
@@ -57,10 +44,9 @@ class Tx_PtExtbase_Tests_Unit_ViewHelpers_Format_FileSizeViewHelperTest extends 
      */
     public function render($input, $useIecLabel, $formatedOutput)
     {
-        $viewHelper =
-            $this->getMockBuilder(Tx_PtExtbase_ViewHelpers_Format_FileSizeViewHelper::class)
-                ->setMethods(['renderChildren'])
-                ->getMock();
+        $viewHelper = $this->getMockBuilder(FileSizeViewHelper::class)
+            ->setMethods(['renderChildren'])
+            ->getMock();
 
         $viewHelper->expects($this->once())->method('renderChildren')->will($this->returnValue($input));
         $actualResult = $viewHelper->render('', $useIecLabel);
