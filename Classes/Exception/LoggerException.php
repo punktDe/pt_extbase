b3c9b0603a0331f5e97953750297fd5645839bb3
Daniel Lienert <daniel@lienert.cc>
2019-09-11 10:40:43 +0200

    FEATURE: Add data field to LoggerException

--- Classes/Exception/LoggerException.php
+++ Classes/Exception/LoggerException.php
@@ -1,28 +1,12 @@
 <?php
 namespace PunktDe\PtExtbase\Exception;
 
-/***************************************************************
- *  Copyright (C) 2014-2016 punkt.de GmbH
- *  Authors: el_equipo <opiuqe_le@punkt.de>
- *
- *  This script is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published
- *  by the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+/*
+ *  (c) 2019 punkt.de GmbH - Karlsruhe, Germany - http://punkt.de
+ *  All rights reserved.
+ */
 
 use TYPO3\CMS\Core\Log\LogLevel;
-
 
 class LoggerException extends \Exception
 {
@@ -32,28 +16,43 @@ class LoggerException extends \Exception
      */
     protected $logLevel;
 
+    /**
+     * @var array
+     */
+    protected $data = [];
 
     /**
      * @param string $message
      * @param int $code
      * @param \Exception|int $logLevel
      * @param \Exception|null $previous
+     * @param array $data
      */
-    public function __construct($message = "", $code = 0, $logLevel = LogLevel::ERROR, \Exception $previous = null)
+    public function __construct($message = '', $code = 0, $logLevel = LogLevel::ERROR, \Exception $previous = null, array $data = [])
     {
         parent::__construct($message, $code, $previous);
+
+        $this->data = $data;
         if (LogLevel::isValidLevel($logLevel)) {
             $this->logLevel = $logLevel;
         } else {
             $this->logLevel = LogLevel::ERROR;
         }
     }
-    
+
     /**
      * @return integer
      */
     public function getLogLevel()
     {
         return $this->logLevel;
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->data;
     }
 }
